c1de1cbe80c1a91aef3e084927d668e5f992af50
SpaceTubeDevs <germain.a13@gmail.com>
2025-11-18 23:05:35 +0100

    generate default resource files

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+name "ls_framework"
+description "This ressource is framework of Lore Santos RP fivem server"
+author "SpaceTube_"
+version "1.0.0"
+
+shared_scripts {
+	'shared/*.lua'
+}
+
+client_scripts {
+	'client/*.lua'
+}
+
+server_scripts {
+	'server/*.lua'
+}
